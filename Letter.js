ef70ab97ff959cad6d59eb10cd920daee7018fb1
David Cheng <dkcheng10@gmail.com>
2018-02-03 19:16:30 -0800

    fixing one problem at a time. Rebuilt Letter function

--- Letter.js
+++ Letter.js
@@ -1,24 +1,36 @@
-var letterToBeGuessed =
+var letter = function (userGuess) {
+       this.value = userGuess;
+       this.show = false;
 
-function (userGuess) {
-    //Created a new variable with set false as the defualt value
-    var isLetterInWord = false;
-    for (var i = 0; i < numBlanks; i++) {
-        if (this.word[i] == userGuess) {
-            isLetterInWord = true;
-        }
+    if(this.value == ' ') {
+
+        this.show = true;
     }
-
-    if (isLetterInWord) {
-        for (var i = 0; i < numBlanks; i++) {
-            if (wordChosen[i] == userGuess) {
-                blankArray[i] = userGuess;
-            }
+    letter.printInfo = function () {
+        if (this.show) {
+            return this.value;
         }
-    } else {
-        wrongGuesses.push(userGuess);
-        this.guesses--;
+        return "_ ";
     }
 }
+    //Created a new variable with set false as the defualt value
+//     var isLetterInWord = false;
+//     for (var i = 0; i < numBlanks; i++) {
+//         if (this.word[i] == userGuess) {
+//             isLetterInWord = true;
+//         }
+//     }
 
-module.exports = letterToBeGuessed;
+//     if (isLetterInWord) {
+//         for (var i = 0; i < numBlanks; i++) {
+//             if (wordChosen[i] == userGuess) {
+//                 blankArray[i] = userGuess;
+//             }
+//         }
+//     } else {
+//         wrongGuesses.push(userGuess);
+//         this.guesses--;
+//     }
+// }
+
+module.exports = letter;
