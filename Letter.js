af36a6cb22e8da9ef56f79d91007db63b75935df
David Cheng <dkcheng10@gmail.com>
2018-02-05 19:09:24 -0800

    Upon reading the directions again and having a eureka moment, game.js is not needed. Will write everthing in index.js

--- Letter.js
+++ Letter.js
@@ -13,24 +13,6 @@ var letter = function (userGuess) {
         return "_ ";
     }
 }
-    //Created a new variable with set false as the defualt value
-//     var isLetterInWord = false;
-//     for (var i = 0; i < numBlanks; i++) {
-//         if (this.word[i] == userGuess) {
-//             isLetterInWord = true;
-//         }
-//     }
 
-//     if (isLetterInWord) {
-//         for (var i = 0; i < numBlanks; i++) {
-//             if (wordChosen[i] == userGuess) {
-//                 blankArray[i] = userGuess;
-//             }
-//         }
-//     } else {
-//         wrongGuesses.push(userGuess);
-//         this.guesses--;
-//     }
-// }
 
 module.exports = letter;
--- index.js
+++ index.js
@@ -1,8 +1,86 @@
-//Requires game file
-var Game = require('./game.js');
-//Creates new object from object class constructor
-var game = new Game();
+//Runs Inquirer to ask the questions
+var inquirer = require("inquirer");
+//requires the word file to get selected words
+var Word = require("./Word.js");
+//requires the letter file to check if you got it right or not
+var Letter = require("./Letter.js");
 //Runs play function inside game to start the game
-game.play();
 
-//Star Wars Lord of the Rings Title Mashup
+
+//Variables that I need
+
+var a;
+var winCount = 0;
+var loseCount = 0;
+var blankArray = [];
+var guessesLeft = 10;
+var wrongGuesses = [];
+var wordChosen = '';
+var lettersInWord = [];
+var numBlanks = 0;
+
+function startGame() {
+    inquirer.prompt({
+        message: "confirm",
+        question: "Shall we play a game?",
+        name: "confirm",
+        default: true
+    }).then(function (confirm) {
+      if (confirm) {
+                guessesLeft = 10;
+                wrongGuesses = [];
+                blankArray = [];
+                wordChosen = Word[Math.floor(Math.random() * Word.length)];
+                lettersInWord = wordChosen.split('');
+                numBlanks = lettersInWord.length;
+                for (var i = 0; i < numBlanks; i++) {
+                    blankArray.push("_");
+                }
+                a = blankArray.join(' ');
+                //Displays the number of blanks
+                //Call inquirer 
+                console.log(a);
+                 function guessALetter () {
+                     inquirer.prompt(
+                         {
+                             type: "input",
+                             message: "Guess a letter.",
+                             name: "letter"
+                         }
+                     ).then(
+                         function letterGuessed (letter) {
+                             var isLetterInWord = false;
+                             for (var i = 0; i < numBlanks; i++) {
+                                 if (wordChosen[i] == letter) {
+                                     isLetterInWord = true;
+                                 }
+                             }
+
+                             if (isLetterInWord) {
+                                 for (var i = 0; i < numBlanks; i++) {
+                                     if (wordChosen[i] == letter) {
+                                         blankArray[i] = letter;
+                                     }
+                                 }
+                             } else {
+                                 wrongGuesses.push(letter);
+                                 guessesLeft--;
+                             }
+                             console.log(a + '\n' + "Number of guesses left:" + guessesLeft + '\n' + "Letters already guessed" + wrongGuesses);
+                             
+                            }
+                     )
+
+                 }
+                        
+
+    
+
+
+      }  else {
+          console.log("Come back later");
+          startGame();
+      }
+    });
+}
+startGame();
